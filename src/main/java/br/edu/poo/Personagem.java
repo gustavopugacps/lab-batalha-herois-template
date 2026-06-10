faa65ce5121717ead1bb61cddce2c4728293b364
gustavopugacps <gustavo.puga@cps.sp.gov.br>
2026-06-09 22:37:59 -0300

    Update Personagem.java
    Criando estrutura básica

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -1,1 +1,35 @@
+package br.edu.poo;
 
+public class Personagem {
+
+    public Personagem(String nome,
+                      int vida,
+                      int ataque,
+                      int defesa) {
+
+    }
+
+    public void atacar(Personagem inimigo) {
+
+    }
+
+    public void receberDano(int dano) {
+
+    }
+
+    public void usarPocao(Pocao pocao) {
+
+    }
+
+    public boolean estaVivo() {
+        return false;
+    }
+
+    public String getNome() {
+        return null;
+    }
+
+    public int getVida() {
+        return 0;
+    }
+}
