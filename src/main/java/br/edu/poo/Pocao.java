a931b6eb0f2276e9a3b395dd0ebef6b69a5a6b94
gustavopugacps <gustavo.puga@cps.sp.gov.br>
2026-06-09 22:37:27 -0300

    Update Pocao.java
    Criando estrutura básica

--- src/main/java/br/edu/poo/Pocao.java
+++ src/main/java/br/edu/poo/Pocao.java
@@ -1,1 +1,16 @@
+package br.edu.poo;
 
+public class Pocao {
+
+    public Pocao(String nome, int cura) {
+
+    }
+
+    public String getNome() {
+        return null;
+    }
+
+    public int getCura() {
+        return 0;
+    }
+}
